3a2cc7c70ead08410da83f75ffcbc50fc281a5c8
Rahul <vermarahulkps@gmail.com>
2018-08-31 15:29:21 +0530

    First Commit

---
+++ ChatApiTryOne/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+
+namespace ChatApiTryOne
+{
+    public class ChatHub:Hub
+    {
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.SendAsync("sendToAll", name, message);
+        }
+
+        public void PrintId(string id)
+        {
+            Clients.All.SendAsync("printId", Context.ConnectionId);
+        }
+
+        public void JoinGroup(string name)
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, "foo");
+        }
+
+        public void LeaveGroup(string name)
+        {
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, "foo");
+        }
+
+         public void SendMessageToGroups(string name,string message)
+        {
+            List<string> groups = new List<string>() { "foo" };
+             Clients.Groups(groups).SendAsync("SendMessageToGroups",name, message);
+        }
+    }
+}
